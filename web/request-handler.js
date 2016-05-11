1bfdea7b070e1ec8d024fcd31c3556a22482c97d
veena-sridhar <v.sridhar91@gmail.com>
2016-05-11 16:45:12 -0700

    Passes first specification to return an html file

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  fs.readFile(path.join(__dirname, './public/index.html'), function(error, content) {
+    console.log('this shows the file', content);
+    if (error) {
+      throw error;
+    } 
+    res.writeHeader(200, {'Content-Type': 'text/html'});
+    res.write(content);
+    res.end();
+  });
+  // res.end(archive.paths.list);
 };
+
+
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,11 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var http = require('http');
+var fs = require('fs');
+
+
+fs.readfile('./index.html', function(html) {
+  response.writeHeader(200, {'Content-Type': 'text/html'});
+  response.write(html);
+  response.end();
+});
